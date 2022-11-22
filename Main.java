4e6d02a27fd19a0245c0823d5490bb483edb5ed9
DanielChakarov <63393633+DanielChakarov@users.noreply.github.com>
2022-11-22 19:46:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
         GarageFrame garageFrame = new GarageFrame();
 
-//        garageFrame.setSize(700,700);
-//        garageFrame.setVisible(true);
-//        garageFrame.pack();
+
     }
 }
